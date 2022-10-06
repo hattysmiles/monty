c6de4207cf2730f01c9356fb5ff9148854fdf4da
hattysmiles <106765765+hattysmiles@users.noreply.github.com>
2022-10-06 23:11:57 +0100

    Update _pop.c

--- _pop.c
+++ _pop.c
@@ -1,22 +1,22 @@
-#include "monty.h"
 /**
- * _pop - function that deletes the first node of the list
- * @stack: the list
- * @line_number: number of line of the command
+ * monty_pop - Removes the top value element of a stack_t linked list.
+ * @stack: A pointer to the top mode node of a stack_t linked list.
+ * @line_number: The current working line number of a Monty bytecodes file.
  */
-
-void _pop(stack_t **stack, unsigned int line_number)
+void monty_pop(stack_t **stack, unsigned int line_number)
 {
-	stack_t *temp = *stack;
+	stack_t *next = NULL;
 
-	if (*stack == NULL)
+	if ((*stack)->next == NULL)
 	{
-		fprintf(stderr, "L%d: can't pop an empty stack\n", line_number);
-		error = 1;
+		set_op_tok_error(pop_error(line_number));
 		return;
 	}
-	*stack = (*stack)->next;
-		if (*stack)
-			(*stack)->prev = NULL;
-		free(temp);
+
+	next = (*stack)->next->next;
+	free((*stack)->next);
+	if (next)
+		next->prev = *stack;
+	(*stack)->next = next;
 }
+
