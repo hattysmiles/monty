718914a1efa55a38f8a90a9fee6e1c44b64084e3
hattysmiles <106765765+hattysmiles@users.noreply.github.com>
2022-10-06 23:08:43 +0100

    Update _pint.c

--- _pint.c
+++ _pint.c
@@ -1,17 +1,18 @@
 #include "monty.h"
 /**
- * _pint - function that prints the value at the top of the stack
- * @stack: the list
- * @line_number: number of line of the command
+ * monty_pint - Prints the top value of a stack_t linked list.
+ * @stack: A pointer to the top mode node of a stack_t linked list.
+ * @line_number: The current working line number of a Monty bytecodes file.
  */
-
-void _pint(stack_t **stack, unsigned int line_number)
+void monty_pint(stack_t **stack, unsigned int line_number)
 {
-	if (*stack == NULL)
+	if ((*stack)->next == NULL)
 	{
-		fprintf(stderr, "L%d: can't pint, stack empty\n", line_number);
-		error = 1;
+		set_op_tok_error(pint_error(line_number));
 		return;
 	}
-	printf("%d\n", (*stack)->n);
+
+	printf("%d\n", (*stack)->next->n);
 }
+
+
